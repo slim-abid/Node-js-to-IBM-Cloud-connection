b853c04086dfe605516782feaebf94ec9466aa10
slim abid <slimabid10@gmail.com>
2021-04-30 18:35:35 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+ - [ ] Depenency installation :
+ - from the terminal run `npm install` to install all the dependencies.
+ 
+ - [ ] Follow the comments in the code to enter your params and credentials.
---
+++ index.js
@@ -0,0 +1,40 @@
+
+if (process.env.NODE_ENV !== 'usingEnv') {
+    require('dotenv').config();
+  }
+var mqtt = require('mqtt')
+var options = {
+    port: 1883, //don't put 8883 because it's encrypted 
+    host: 'mqtt://' + '', //put your ibm cloud host in the empty string
+    clientId: '' + Math.random().toString(16).substr(2, 8), // a:orgId: in the empty string
+    username: '', // your API key
+    password: '', // your API token
+    keepalive: 60,
+    reconnectPeriod: 1000,
+    protocolId: 'MQIsdp',
+    protocolVersion: 3,
+    clean: true,
+    encoding: 'utf8'
+};
+var client  = mqtt.connect(options.host, options) 
+var data = {msgIs : 'node js app is here'};
+var data = JSON.stringify(data);
+client.on('connect', function () {
+  
+  client.subscribe('iot-2/type/ESP8266/id/dev01/evt/status/fmt/json', function (err) {
+    if (!err) {
+        console.log('subscribe done succesfully!');
+        client.publish('iot-2/type/ESP8266/id/dev01/evt/status/fmt/json', data)
+    }
+  })
+
+})
+ 
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  
+})
+client.on('error', function(err) {
+    console.log(err);
+});
